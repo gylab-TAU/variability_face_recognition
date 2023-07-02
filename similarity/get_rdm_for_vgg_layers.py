e24cf51856ba90fee9d0ab4a8abda240adef4081
shiri <shirialmog1@gmail.com>
2023-07-02 07:55:49 +0300

    add fc8, save embeddings as npy

--- similarity/get_rdm_for_vgg_layers.py
+++ similarity/get_rdm_for_vgg_layers.py
@@ -1,17 +1,23 @@
 import os
 from argparse import ArgumentParser
-from rdm import load_data, get_embeddings, get_rdm, save_rdm_matrix_with_names
+import numpy as np
 import matplotlib.pyplot as plt
+
+from rdm import load_data, get_embeddings, get_rdm, save_rdm_matrix_with_names
+
 
 layers_dict = {'conv1': ['features.module.4', 802816],
                'conv2': ['features.module.9', 401408],
                'conv3': ['features.module.16', 200704],
                'conv4': ['features.module.23', 100352],
                'conv5': ['features.module.30', 25088],
-               'fc7': ['classifier.4', 4096],
-               'fc6': ['classifier.1', 4096], }
+               'fc6': ['classifier.4', 4096],
+               'fc7': ['classifier.1', 4096],
+               'fc8': ['classifier.6', 8749]}
 
 if __name__ == '__main__':
+    x = np.load(r"C:\Users\shiri\Documents\School\Master\data\galit_rdm\results_auto\VGG16_features.module.4_embeddings.npy")
+
     parser = ArgumentParser()
     parser.add_argument("-data_dir", "--data_dir", dest="data_dir", help="folder with data")
     parser.add_argument("-model_path", "--model_path", dest="model_path", help="path to model weights", required=False)
@@ -26,10 +32,16 @@ if __name__ == '__main__':
         layer_size = layers_dict[layer][1]
         model_embeddings = get_embeddings(data_paths_list, model_type, args.model_path, perform_mtcnn=True,
                                           layer_name=layer_name, layer_size=layer_size)
+
+        #save embeddings
+        np.save(os.path.join(args.output_path, f'VGG16_{layer_name}_embeddings.npy'), model_embeddings)
+
+        #save rdm
         rdm_matrix = get_rdm(model_embeddings)
         save_path = os.path.join(args.output_path, f'RDM_VGG16_{layer}.csv')
         save_rdm_matrix_with_names(rdm_matrix, names_list, save_path)
 
+        #save rdm image
         plt.imshow(rdm_matrix)
         plt.title(f'RDM {layer}')
         plt.savefig(os.path.join(args.output_path, f'RDM_VGG16_{layer}.png'))
